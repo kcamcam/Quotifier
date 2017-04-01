b817982aa5808045132c30d59f37e77a8ea93e69
Kevin Camellini <kevincamellini@gmail.com>
2017-03-31 23:34:44 -0400

    quotify.php

--- quotify.php
+++ quotify.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <!--META DATA -->
+    <title>Quotifier</title>
+    <link rel="icon" type="image/png" href="doublequotes.png"/>
+    <meta name="author" content="Kevin Camellini">
+    <meta name="description" content="Comment your code with random quotes.">
+    <meta name="keywords" content="comments, quotes, code, github, bootstrap, software, develoeper, gandhi">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- FontAwesome -->
+    <script src="https://use.fontawesome.com/e0af25fb51.js"></script>
+    <!-- Custom Style Sheet -->
+    <link href="css/style2.css" rel="stylesheet" type="text/css">
+</head>
+<body onload="setYear()">
+    <!-- Body -->
+    <div class="landing">
+        <div class="container-fluid">
+            <div class="row-fluid">
 <?php
     /* ----- QUOTIFIER VARIABLES ------ */
     $jsonfile = file_get_contents("db.json");
@@ -150,3 +173,34 @@
         echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
     }
 ?>
+            </div>
+        </div>
+    </div>
+    <!-- Footer -->
+    <div class="footer text-center">
+        <div class="container-fluid">
+            <div class="row-fluid social">
+                <a href="https://twitter.com/famingolabs" target="_blank">
+                    <span class="fa fa-twitter"></span>
+                </a>
+                <a href="mailto:hello@famingolabs.com?Subject=Quotifier%20app">
+                    <span class="fa fa-envelope-o"></span>
+                </a>
+            </div>
+            <div class="row-fluid famingo">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="padding: 0 0;">
+                    <p class="text-muted"><a href="https://www.famingolabs.com" target="_blank">&copy; <span id="yearFooter"></span> Famingo Labs Inc.</a> All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- jQuery JavaScript -->
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous">
+    </script>
+    <!-- Bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
+    <!-- Custom Scripts -->
+    <script type="text/javascript" src="script/script.js"></script>
+</body>
+</html>

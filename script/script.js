8c9d5a64aefd3ee8fe0d4f547928f0807f3f3c4d
Kevin Camellini <kevincamellini@gmail.com>
2017-04-13 22:16:18 -0400

    added genquote function

--- script/script.js
+++ script/script.js
@@ -6,6 +6,17 @@ function setYear(){
     date = "20"+ date.substring(1,3);
     footer.innerHTML=date;
 }
+/* Genereate a Random Quote */
+function genquote(){
+    var codebox = document.getElementById("quote");
+    var smajsonQuotes = db['quotes'];
+    var smajsonAuthors = db['authors'];
+    var x = Math.floor((Math.random()*smajsonQuotes.length)+0);
+    var y = Math.floor((Math.random()*smajsonAuthors.length)+0);
+    var ghandi = "//\""+smajsonQuotes[x]+"\" - "+smajsonAuthors[y];
+    codebox.innerHTML += "<code>"+ghandi+"</code><br/><br/>";
+}
+
 /*File Upload Button */
 $('#file-upload').change(function() {
     var i = $(this).prev('label').clone();
@@ -26,6 +37,7 @@ $(document).ready(
         );
     }
 );
+
 /*---- Google Analytics ----*/
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
